1595c8fdb422bb62d218f29d93f47c818540c516
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-12-22 15:37:16 +0900

    apply pylint linter and black formatter to codes

--- 03_sort/cocktail.py
+++ 03_sort/cocktail.py
@@ -1,33 +1,37 @@
 from typing import List
 
+
 def cocktail_sort(numbers: List[int]) -> List[int]:
-  len_numbers = len(numbers)
-  swapped = True
-  start_index = 0
-  end_index = len_numbers - 1
-  while swapped:
-      swapped = False
-      for i in range(start_index, end_index):
-        if numbers[i] > numbers[i+1]:
-          numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
-          swapped = True
+    len_numbers = len(numbers)
+    swapped = True
+    start_index = 0
+    end_index = len_numbers - 1
+    while swapped:
+        swapped = False
+        for i in range(start_index, end_index):
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+                swapped = True
 
-      if not swapped:
-          break
+        if not swapped:
+            break
 
-      swapped = False
-      end_index = end_index - 1
+        swapped = False
+        end_index = end_index - 1
 
-      for i in range(end_index-1, start_index-1, -1): # for example, loop 4 times, decreasing by -1 from 3 until it reaches -1.
-        if numbers[i] > numbers[i+1]:
-          numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
-          swapped = True
+        for i in range(
+            end_index - 1, start_index - 1, -1
+        ):  # for example, loop 4 times, decreasing by -1 from 3 until it reaches -1.
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+                swapped = True
 
-      start_index = start_index + 1
-  return numbers
+        start_index = start_index + 1
+    return numbers
 
 
-if __name__ == '__main__':
-  import random
-  nums = [random.randint(0, 1000) for i in range(10)]
-  print(cocktail_sort(nums))
+if __name__ == "__main__":
+    import random
+
+    nums = [random.randint(0, 1000) for i in range(10)]
+    print(cocktail_sort(nums))

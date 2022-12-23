9778aa37eb9afc6ae89d7cb953bb8293118e21b9
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-12-23 10:39:30 +0900

    solution for single_number

---
+++ leetcode/single_number.py
@@ -0,0 +1,27 @@
+from typing import List
+
+# https://leetcode.com/problems/single-number/submissions/
+def singleNumber(nums: List[int]) -> int:
+    uniq_num = 0
+    for i in nums:
+        uniq_num ^= i
+    # for example, nums is assumed as [1, 2, 4, 2, 1]
+    # uniq_num xor i will be expressed bitwise below
+    # i = 1
+    #   (000)2 xor (001)2 = (001)2 = (1)10
+    # i = 2
+    #   (001)2 xor (010)2 = (011)2 = (3)10
+    # i = 4
+    #   (011)2 xor (100)2 = (111)2 = (7)10
+    # i = 2
+    #   (111)2 xor (010)2 = (101)2 = (5)10
+    # i = 1
+    #   (101)2 xor (001)2 = (100)2 = (4)10
+    return uniq_num
+
+
+if __name__ == "__main__":
+
+    numbers = [1, 2, 4, 2, 1]
+    print(numbers)
+    print(singleNumber(numbers))

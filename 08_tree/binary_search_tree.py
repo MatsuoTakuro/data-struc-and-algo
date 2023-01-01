13e538ef6b03249d5f5cdbbdf6fb0c6cf9776c8e
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2023-01-01 10:20:37 +0900

    search value using binary tree

--- 08_tree/binary_search_tree.py
+++ 08_tree/binary_search_tree.py
@@ -19,6 +19,31 @@ def insert(node: Node, value: Any) -> Node:
     return node
 
 
+# in_order: left -> root -> right
+# pre_order: root -> left -> right
+# post_order: left -> right -> root
+def in_order(node: Node) -> None:
+    if node is not None:
+        in_order(node.left)
+
+        print(node.value)
+        in_order(node.right)
+
+
+def search(node: Node, value: Any) -> bool:
+    if node is None:
+        return False
+
+    if value < node.value:
+        return search(node.left, value)
+
+    if value == node.value:
+        return True
+
+    if node.value < value:
+        return search(node.right, value)
+
+
 if __name__ == "__main__":
     root = insert(None, 3)
     root = insert(root, 6)
@@ -27,4 +52,7 @@ if __name__ == "__main__":
     root = insert(root, 1)
     root = insert(root, 10)
     root = insert(root, 2)
-    print(root.left.right.value)
+    in_order(root)
+    print(search(root, 2))
+    print(search(root, 5))
+    print(search(root, 4))
